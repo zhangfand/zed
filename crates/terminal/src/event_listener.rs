e0b44588f93aafb52d9018b8ef078fc14ada1d4d
Mikayla Maki <mikayla.c.maki@icloud.com>
2022-06-16 16:42:25 -0700

    Can read keys :D

--- crates/terminal/src/event_listener.rs
+++ crates/terminal/src/event_listener.rs
@@ -1,28 +1,13 @@
-use std::sync::Arc;
-
 use alacritty_terminal::event::{Event, EventListener};
-use gpui::ViewContext;
-use workspace::Workspace;
 
 ///This module translates alacritty_terminal events to their Zed equivalents
 
 ///The event loop
-#[derive(Clone, Copy)]
-pub struct ZedTerminalHandle {}
+#[derive(Clone)]
+pub struct ZedTerminalHandle(pub futures::channel::mpsc::UnboundedSender<Event>);
+
 impl EventListener for ZedTerminalHandle {
     fn send_event(&self, event: Event) {
-        match event {
-            Event::MouseCursorDirty => dbg!("MouseCursorDirty"),
-            Event::Title(t) => dbg!("???"),
-            Event::ResetTitle => dbg!("ResetTitle"),
-            Event::ClipboardStore(_, _) => dbg!("ClipboardStore"),
-            Event::ClipboardLoad(_, _) => dbg!("ClipboardLoad"),
-            Event::ColorRequest(_, _) => dbg!("ColorRequest"),
-            Event::PtyWrite(_) => dbg!("PtyWrite"),
-            Event::CursorBlinkingChange => dbg!("CursorBlinkingChange"),
-            Event::Wakeup => dbg!("Wakeup"), //Trigger re-render
-            Event::Bell => dbg!("Bell"),
-            Event::Exit => dbg!("Exit"),
-        };
+        self.0.unbounded_send(event).ok();
     }
 }
--- crates/terminal/src/terminal.rs
+++ crates/terminal/src/terminal.rs
@@ -1,70 +1,72 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use alacritty_terminal::{
     config::{Config, Program, PtyConfig},
     event::Notify,
-    event_loop::{EventLoop, Msg, Notifier},
-    grid::Dimensions,
+    event_loop::{EventLoop, Notifier},
     sync::FairMutex,
     term::SizeInfo,
     tty, Term,
 };
 use event_listener::ZedTerminalHandle;
+use futures::StreamExt;
 use gpui::{
     actions,
     color::Color,
     elements::*,
     fonts::{with_font_cache, TextStyle},
     geometry::{rect::RectF, vector::vec2f},
+    impl_internal_actions,
     text_layout::Line,
     Entity, MutableAppContext, View, ViewContext,
 };
-use mio_extras::channel::Sender;
 use project::{Project, ProjectPath};
 use settings::Settings;
 use smallvec::SmallVec;
-use util::ResultExt;
 use workspace::{Item, Workspace};
 
 mod event_listener;
+
+#[derive(Clone, Default, Debug, PartialEq, Eq)]
+struct KeyInput(String);
 
 //Action steps:
 //Create an action struct with actions!
 //Create an action handler that accepts that struct as an arg
 //Register that handler in `init`
 //If adding to key map file, reference the *struct name*, not the *handler function*
-actions!(terminal, [Deploy, FakeSendToPtyPlsDelete]);
+actions!(terminal, [Deploy]); //This is a shortcut for unit structs
+impl_internal_actions!(terminal, [KeyInput]); //For actions that don't need to be serialized
 
 pub fn init(cx: &mut MutableAppContext) {
-    cx.add_action(ZedTerminal::deploy);
-    cx.add_action(ZedTerminal::fake_send_to_pty);
+    cx.add_action(TerminalView::deploy);
+    cx.add_action(TerminalView::handle_key_input);
 }
-pub struct ZedTerminal {
+
+struct TerminalView {
     loop_tx: Notifier,
     term: Arc<FairMutex<Term<ZedTerminalHandle>>>,
     title: String,
 }
 
-impl ZedTerminal {
-    fn new() -> ZedTerminal {
-        //Basic Alacritty terminal architecture:
-        //- Need to create an alacritty_terminal::event::EventListener impl
-        //  (so the terminal can control title & such)
-        //- Need to use their alacritty::tty::new() stuff to spin up a terminal
-        //  + Details like setting up enviroment variables in sub process
-        //- Need to create and store a logical terminal (alacritty_terminal::term::Term)
-        //- Then when rendering, query the logical terminal and draw the output somehow
-        //- Also need to create an event loop for the PTY I/O, term looks for the same
-        //  PtyConfig as tty::new(), so they're communicating through kernel
-        //-Hints:
-        //- Look at alacritty::WindowContext::new() for how to wire things up together
-        //- Look at display for hints on how to query Terminal
+impl Entity for TerminalView {
+    type Event = ();
+}
 
-        //Goals:
-        //Not just a crappy terminal,
-        //Full zed features like collaboration, multicursor, etc.
+impl TerminalView {
+    fn new(cx: &mut ViewContext<Self>) -> Self {
+        let (events_tx, mut events_rx) = futures::channel::mpsc::unbounded();
+        cx.spawn(|this, mut cx| async move {
+            while let Some(event) = events_rx.next().await {
+                this.update(&mut cx, |this, cx| {
+                    this.process_terminal_event(event, cx);
+                    cx.notify();
+                });
+            }
+        })
+        .detach();
 
-        let zed_proxy = ZedTerminalHandle {};
+        let zed_proxy = ZedTerminalHandle(events_tx);
 
         let pty_config = PtyConfig {
             shell: Some(Program::Just("zsh".to_string())),
@@ -84,57 +86,45 @@ impl ZedTerminal {
 
         let pty = tty::new(&pty_config, &size_info, None).expect("Could not create tty");
 
-        let event_loop = EventLoop::new(
-            Arc::clone(&term),
-            zed_proxy.clone(),
-            pty,
-            pty_config.hold,
-            false,
-        );
+        let event_loop =
+            EventLoop::new(term.clone(), zed_proxy.clone(), pty, pty_config.hold, false);
 
         //This variable is how we send stuff to Alacritty
         //Need to wrap it up in a message, which is done by Notifier
         let loop_tx = Notifier(event_loop.channel());
         let _io_thread = event_loop.spawn();
 
-        ZedTerminal {
-            loop_tx,
-            term,
+        // let term = Arc::new(Mutex::new(ZedTerminal::new()));
+        // cx.set_global(term.clone());
+        TerminalView {
             title: "Terminal".to_string(),
+            term,
+            loop_tx,
         }
     }
 
     fn deploy(workspace: &mut Workspace, _: &Deploy, cx: &mut ViewContext<Workspace>) {
-        let project = workspace.project().clone();
-        if project.read(cx).is_remote() {
-            cx.propagate_action();
-        } else if let Some(_) = project
-            .update(cx, |project, cx| project.create_buffer("", None, cx))
-            .log_err()
-        {
-            let term = Arc::new(Mutex::new(ZedTerminal::new()));
-            cx.set_global(term.clone());
-            workspace.add_item(Box::new(cx.add_view(|_cx| TerminalView { term })), cx);
-        }
+        workspace.add_item(Box::new(cx.add_view(|cx| TerminalView::new(cx))), cx);
     }
 
-    fn fake_send_to_pty(
-        workspace: &mut Workspace,
-        _: &FakeSendToPtyPlsDelete,
-        cx: &mut ViewContext<Workspace>,
+    fn process_terminal_event(
+        &mut self,
+        event: alacritty_terminal::event::Event,
+        cx: &mut ViewContext<Self>,
     ) {
-        let term = &cx.global::<Arc<Mutex<ZedTerminal>>>().lock().unwrap();
-        dbg!(&term.title);
-        term.loop_tx.notify("M".as_bytes());
+        match event {
+            alacritty_terminal::event::Event::Wakeup => cx.notify(),
+            alacritty_terminal::event::Event::PtyWrite(out) => {
+                self.loop_tx.notify(out.into_bytes())
+            }
+            _ => {}
+        }
+        //
     }
-}
 
-struct TerminalView {
-    term: Arc<Mutex<ZedTerminal>>,
-}
-
-impl Entity for TerminalView {
-    type Event = ();
+    fn handle_key_input(&mut self, action: &KeyInput, _: &mut ViewContext<Self>) {
+        self.loop_tx.notify(action.0.clone().into_bytes());
+    }
 }
 
 impl View for TerminalView {
@@ -143,9 +133,9 @@ impl View for TerminalView {
     }
 
     fn render(&mut self, cx: &mut gpui::RenderContext<'_, Self>) -> ElementBox {
-        let theme = cx.global::<Settings>().theme.clone();
+        let _theme = cx.global::<Settings>().theme.clone();
 
-        TerminalEl::new(self.term.lock().unwrap().term.clone())
+        TerminalEl::new(self.term.clone())
             .contained()
             // .with_style(theme.terminal.container)
             .boxed()
@@ -153,12 +143,12 @@ impl View for TerminalView {
 }
 
 struct TerminalEl {
-    grid_data: Arc<FairMutex<Term<ZedTerminalHandle>>>,
+    term: Arc<FairMutex<Term<ZedTerminalHandle>>>,
 }
 
 impl TerminalEl {
     fn new(term: Arc<FairMutex<Term<ZedTerminalHandle>>>) -> TerminalEl {
-        TerminalEl { grid_data: term }
+        TerminalEl { term }
     }
 }
 
@@ -177,7 +167,7 @@ impl Element for TerminalEl {
         cx: &mut gpui::LayoutContext,
     ) -> (gpui::geometry::vector::Vector2F, Self::LayoutState) {
         let line = self
-            .grid_data
+            .term
             .lock()
             .grid()
             .display_iter()
@@ -235,15 +225,22 @@ impl Element for TerminalEl {
 
     fn dispatch_event(
         &mut self,
-        _event: &gpui::Event,
+        event: &gpui::Event,
         _bounds: gpui::geometry::rect::RectF,
         _visible_bounds: gpui::geometry::rect::RectF,
         _layout: &mut Self::LayoutState,
         _paint: &mut Self::PaintState,
-        _cx: &mut gpui::EventContext,
+        cx: &mut gpui::EventContext,
     ) -> bool {
-        false
-        // unreachable!("Should never be called hopefully")
+        if let gpui::Event::KeyDown {
+            input: Some(input), ..
+        } = event
+        {
+            cx.dispatch_action(KeyInput(input.clone()));
+            true //Return true if you handled it and need to stop bubbling
+        } else {
+            false
+        }
     }
 
     fn debug(
@@ -265,7 +262,7 @@ impl Item for TerminalView {
         let search_theme = &settings.theme.search;
         Flex::row()
             .with_child(
-                Label::new(self.term.lock().unwrap().title.clone(), style.label.clone())
+                Label::new(self.title.clone(), style.label.clone())
                     .aligned()
                     .contained()
                     .with_margin_left(search_theme.tab_icon_spacing)
