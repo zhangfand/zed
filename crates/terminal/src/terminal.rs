9ea63a0c17833d1639105b5e9fc71b7c545d726f
Mikayla Maki <mikayla.c.maki@icloud.com>
2022-06-17 16:10:41 -0700

    Finished hacking on terminal

--- crates/terminal/src/terminal.rs
+++ crates/terminal/src/terminal.rs
@@ -1,15 +1,21 @@
 use std::sync::Arc;
 
+use alacritty_terminal::ansi::Handler;
+use alacritty_terminal::grid::Scroll;
 use alacritty_terminal::{
     config::{Config, Program, PtyConfig},
-    event::Notify,
+    event::{Event, EventListener, Notify},
     event_loop::{EventLoop, Notifier},
+    grid::Indexed,
+    index::Point,
     sync::FairMutex,
-    term::SizeInfo,
+    term::{cell::Cell, SizeInfo},
     tty, Term,
 };
-use event_listener::ZedTerminalHandle;
-use futures::StreamExt;
+use futures::{
+    channel::mpsc::{unbounded, UnboundedSender},
+    StreamExt,
+};
 use gpui::{
     actions,
     color::Color,
@@ -17,8 +23,11 @@ use gpui::{
     fonts::{with_font_cache, TextStyle},
     geometry::{rect::RectF, vector::vec2f},
     impl_internal_actions,
+    keymap::Keystroke,
     text_layout::Line,
-    Entity, MutableAppContext, View, ViewContext,
+    Entity,
+    Event::KeyDown,
+    MutableAppContext, View, ViewContext,
 };
 use project::{Project, ProjectPath};
 use settings::Settings;
@@ -29,24 +38,37 @@ mod event_listener;
 
 #[derive(Clone, Default, Debug, PartialEq, Eq)]
 struct KeyInput(String);
-
+// #[derive(Clone, Default, Debug, PartialEq, Eq)]
+// struct Backspace;
 //Action steps:
 //Create an action struct with actions!
 //Create an action handler that accepts that struct as an arg
 //Register that handler in `init`
 //If adding to key map file, reference the *struct name*, not the *handler function*
-actions!(terminal, [Deploy]); //This is a shortcut for unit structs
+actions!(terminal, [Deploy, Backspace, Execute]); //This is a shortcut for unit structs
 impl_internal_actions!(terminal, [KeyInput]); //For actions that don't need to be serialized
 
 pub fn init(cx: &mut MutableAppContext) {
     cx.add_action(TerminalView::deploy);
     cx.add_action(TerminalView::handle_key_input);
+    cx.add_action(TerminalView::backspace);
+    cx.add_action(TerminalView::execute);
+}
+
+#[derive(Clone)]
+pub struct ZedListener(UnboundedSender<Event>);
+
+impl EventListener for ZedListener {
+    fn send_event(&self, event: Event) {
+        self.0.unbounded_send(event).ok();
+    }
 }
 
 struct TerminalView {
     loop_tx: Notifier,
-    term: Arc<FairMutex<Term<ZedTerminalHandle>>>,
+    term: Arc<FairMutex<Term<ZedListener>>>,
     title: String,
+    cur_command: String,
 }
 
 impl Entity for TerminalView {
@@ -55,8 +77,9 @@ impl Entity for TerminalView {
 
 impl TerminalView {
     fn new(cx: &mut ViewContext<Self>) -> Self {
-        let (events_tx, mut events_rx) = futures::channel::mpsc::unbounded();
+        let (events_tx, mut events_rx) = unbounded();
         cx.spawn(|this, mut cx| async move {
+            //TODO cleanup
             while let Some(event) = events_rx.next().await {
                 this.update(&mut cx, |this, cx| {
                     this.process_terminal_event(event, cx);
@@ -66,33 +89,35 @@ impl TerminalView {
         })
         .detach();
 
-        let zed_proxy = ZedTerminalHandle(events_tx);
-
         let pty_config = PtyConfig {
             shell: Some(Program::Just("zsh".to_string())),
             working_directory: None,
             hold: false,
         };
 
-        // TODO: Modify settings to populate the alacritty config
         let config = Config {
             pty_config: pty_config.clone(),
             ..Default::default()
         };
-        let size_info = SizeInfo::new(100., 100., 5., 5., 0., 0., false);
+        let size_info = SizeInfo::new(400., 100.0, 5., 5., 0., 0., false);
 
-        let term = Term::new(&config, size_info, zed_proxy.clone());
+        let term = Term::new(&config, size_info, ZedListener(events_tx.clone()));
         let term = Arc::new(FairMutex::new(term));
 
         let pty = tty::new(&pty_config, &size_info, None).expect("Could not create tty");
 
-        let event_loop =
-            EventLoop::new(term.clone(), zed_proxy.clone(), pty, pty_config.hold, false);
+        let event_loop = EventLoop::new(
+            term.clone(),
+            ZedListener(events_tx.clone()),
+            pty,
+            pty_config.hold,
+            false,
+        );
 
         //This variable is how we send stuff to Alacritty
         //Need to wrap it up in a message, which is done by Notifier
         let loop_tx = Notifier(event_loop.channel());
-        let _io_thread = event_loop.spawn();
+        let _io_thread = event_loop.spawn(); //todo cleanup
 
         // let term = Arc::new(Mutex::new(ZedTerminal::new()));
         // cx.set_global(term.clone());
@@ -100,6 +125,7 @@ impl TerminalView {
             title: "Terminal".to_string(),
             term,
             loop_tx,
+            cur_command: "".to_string(),
         }
     }
 
@@ -122,8 +148,27 @@ impl TerminalView {
         //
     }
 
-    fn handle_key_input(&mut self, action: &KeyInput, _: &mut ViewContext<Self>) {
-        self.loop_tx.notify(action.0.clone().into_bytes());
+    //
+    fn handle_key_input(&mut self, action: &KeyInput, cx: &mut ViewContext<Self>) {
+        let c = action.0.chars().next().unwrap();
+        let mut bytes = vec![0; c.len_utf8()];
+        c.encode_utf8(&mut bytes[..]);
+        self.loop_tx.notify(bytes);
+    }
+
+    fn backspace(&mut self, action: &Backspace, cx: &mut ViewContext<Self>) {
+        let c = 127u8 as char;
+        let mut bytes = vec![0; c.len_utf8()];
+        c.encode_utf8(&mut bytes[..]);
+        self.loop_tx.notify(bytes);
+    }
+
+    fn execute(&mut self, action: &Execute, cx: &mut ViewContext<Self>) {
+        let c = 13u8 as char;
+        let mut bytes = vec![0; c.len_utf8()];
+        c.encode_utf8(&mut bytes[..]);
+        self.loop_tx.notify(bytes);
+        self.term.lock().scroll_display(Scroll::Bottom);
     }
 }
 
@@ -143,11 +188,11 @@ impl View for TerminalView {
 }
 
 struct TerminalEl {
-    term: Arc<FairMutex<Term<ZedTerminalHandle>>>,
+    term: Arc<FairMutex<Term<ZedListener>>>,
 }
 
 impl TerminalEl {
-    fn new(term: Arc<FairMutex<Term<ZedTerminalHandle>>>) -> TerminalEl {
+    fn new(term: Arc<FairMutex<Term<ZedListener>>>) -> TerminalEl {
         TerminalEl { term }
     }
 }
@@ -166,22 +211,37 @@ impl Element for TerminalEl {
         constraint: gpui::SizeConstraint,
         cx: &mut gpui::LayoutContext,
     ) -> (gpui::geometry::vector::Vector2F, Self::LayoutState) {
-        let line = self
-            .term
-            .lock()
-            .grid()
-            .display_iter()
-            .map(|c| c.c)
-            .collect::<String>();
-        dbg!(&line);
+        let term = self.term.lock();
+        let content = term.renderable_content();
+
+        let mut lines = vec![];
+        let mut cur_line = vec![];
+        let mut last_line = 0;
+        for cell in content.display_iter {
+            let Indexed {
+                point: Point { line, .. },
+                cell: Cell { c, .. },
+            } = cell;
+
+            if line != last_line {
+                lines.push(cur_line);
+                cur_line = vec![];
+                last_line = line.0;
+            }
+            cur_line.push(c);
+        }
+        let line = lines
+            .into_iter()
+            .map(|char_vec| char_vec.into_iter().collect::<String>())
+            .fold("".to_string(), |grid, line| grid + &line + "\n");
+
         let chunks = vec![(&line[..], None)].into_iter();
 
         let text_style = with_font_cache(cx.font_cache.clone(), || TextStyle {
             color: Color::white(),
             ..Default::default()
-        }); //Here it's 14?
+        });
 
-        //Nescessary to send the
         let shaped_lines = layout_highlighted_chunks(
             chunks,
             &text_style,
@@ -209,11 +269,9 @@ impl Element for TerminalEl {
         cx: &mut gpui::PaintContext,
     ) -> Self::PaintState {
         let mut origin = bounds.origin();
-        dbg!(layout.line_height);
 
         for line in &layout.lines {
             let boundaries = RectF::new(origin, vec2f(bounds.width(), layout.line_height));
-            dbg!(origin.y(), boundaries.max_y());
 
             if boundaries.intersects(visible_bounds) {
                 line.paint(origin, visible_bounds, layout.line_height, cx);
@@ -232,14 +290,29 @@ impl Element for TerminalEl {
         _paint: &mut Self::PaintState,
         cx: &mut gpui::EventContext,
     ) -> bool {
-        if let gpui::Event::KeyDown {
-            input: Some(input), ..
-        } = event
-        {
-            cx.dispatch_action(KeyInput(input.clone()));
-            true //Return true if you handled it and need to stop bubbling
-        } else {
-            false
+        match event {
+            KeyDown {
+                input: Some(input), ..
+            } => {
+                cx.dispatch_action(KeyInput(input.clone()));
+                true
+            }
+            KeyDown {
+                keystroke: Keystroke { key, .. },
+                input: None,
+                ..
+            } => {
+                if key == "backspace" {
+                    cx.dispatch_action(Backspace);
+                    true
+                } else if key == "enter" {
+                    cx.dispatch_action(Execute);
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => false,
         }
     }
 
