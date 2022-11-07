65215ce8cfaaf2067a63c34cb14da218a3f52203
Antonio Scandurra <me@as-cii.com>
2022-11-07 14:27:55 +0100

    Add table for calls

--- crates/collab/migrations/20221107110425_reconnection_support.sql
+++ crates/collab/migrations/20221107110425_reconnection_support.sql
@@ -13,18 +13,29 @@ CREATE TABLE IF NOT EXISTS "rooms" (
 
 CREATE TABLE IF NOT EXISTS "room_participants" (
     "id" SERIAL PRIMARY KEY,
-    "room_id" INTEGER NOT NULL REFERENCES rooms (id) ON DELETE CASCADE,
+    "room_id" INTEGER NOT NULL REFERENCES rooms (id),
     "user_id" INTEGER NOT NULL REFERENCES users (id),
-    "peer_id" INTEGER,
     "location_kind" INTEGER,
     "location_project_id" INTEGER REFERENCES projects (id), 
+    "peer_id" INTEGER,
     "server_epoch" UUID NOT NULL
 );
 CREATE UNIQUE INDEX "index_room_participants_on_user_id_and_room_id" ON "room_participants" ("user_id", "room_id");
 
 CREATE TABLE IF NOT EXISTS "room_participant_projects" (
     "id" SERIAL PRIMARY KEY,
-    "room_participant_id" INTEGER NOT NULL REFERENCES room_participants (id) ON DELETE CASCADE,
+    "participant_id" INTEGER NOT NULL REFERENCES room_participants (id),
     "project_id" INTEGER NOT NULL REFERENCES projects (id)
 );
-CREATE INDEX "index_room_participant_projects_on_room_participant_id" ON "room_participant_projects" ("room_participant_id");
+CREATE INDEX "index_room_participant_projects_on_participant_id" ON "room_participant_projects" ("participant_id");
+
+CREATE TABLE IF NOT EXISTS "calls" (
+    "id" SERIAL PRIMARY KEY,
+    "caller_user_id" INTEGER NOT NULL REFERENCES users (id),
+    "callee_user_id" INTEGER NOT NULL REFERENCES users (id),
+    "room_id" INTEGER NOT NULL REFERENCES rooms (id),
+    "initial_project_id" INTEGER REFERENCES projects (id),
+    "peer_id" INTEGER,
+    "server_epoch" UUID NOT NULL
+);
+CREATE UNIQUE INDEX "index_calls_on_callee_user_id" ON "calls" ("callee_user_id");
