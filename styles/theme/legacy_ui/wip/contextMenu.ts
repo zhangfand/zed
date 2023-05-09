bfa53537f7aec7a1f41646472ba1dbd89c6ddf7e
Nate Butler <iamnbutler@gmail.com>
2023-05-08 23:48:07 -0400

    WIP

--- styles/theme/legacy_ui/wip/contextMenu.ts
+++ styles/theme/legacy_ui/wip/contextMenu.ts
@@ -1,70 +1,51 @@
-import { border } from "@theme/border"
-import { Theme } from "@theme/config"
-import { weight } from "@theme/text"
+import { Theme } from "@theme";
+import { containedText, containerStyle } from "@theme/container"
+import { popoverContainerStyle } from "@theme/container/popover"
+import { margin, padding } from "@theme/properties"
+import { interactiveTextStyle, weight } from "@theme/text"
 
 export default function contextMenu(theme: Theme) {
-    const container = {}
+  const KEYSTROKE_MARGIN: Readonly<number> = 30;
+  const ICON_SIZE: Readonly<number> = 8;
+  const ICON_SPACING: Readonly<number> = 14;
 
-    const legacy_properties = {
-        keystrokeMargin: 30,
+  const container = popoverContainerStyle({ theme })
+
+  const item = interactiveTextStyle(theme, {
+    padding: padding(6, 2),
+    borderRadius: 6
+  })
+
+  const keystroke = containedText({
+    theme, options: {
+      weight: weight.bold,
     }
+  })
 
-    // container()
-    // flex() // container with flex
-    // interactiveFlex() // interactive<container> with flex
-    // shadow() or add shdow to container
+  const separator = containerStyle({
+    background: container.border.color,
+    margin: margin(0, 2)
+  })
 
-    const containerStyle = container(theme, {
-        background: background(theme, "popover"),
-        borderRadius: 10,
-        padding: 4,
-        border: border(theme, "popover"),
-        shadow: shadow(theme),
-    })
-
-    const item = interactiveFlex(theme, {})
-
-    const keystroke = label(theme, {
-        weight: weight.bold,
-    })
-
-    return {
-        ...legacy_properties,
-        ...containerStyle,
-        item: {
-            iconSpacing: 8,
-            iconWidth: 14,
-            padding: { left: 6, right: 6, top: 2, bottom: 2 },
-            cornerRadius: 6,
-            label: text(layer, "sans", { size: "sm" }),
-            keystroke: {
-                ...text(layer, "sans", "variant", {
-                    size: "sm",
-                    weight: "bold",
-                }),
-                padding: { left: 3, right: 3 },
-            },
-            hover: {
-                background: background(layer, "hovered"),
-                label: text(layer, "sans", "hovered", { size: "sm" }),
-                keystroke: {
-                    ...text(layer, "sans", "hovered", {
-                        size: "sm",
-                        weight: "bold",
-                    }),
-                    padding: { left: 3, right: 3 },
-                },
-            },
-            active: {
-                background: background(layer, "active"),
-            },
-            activeHover: {
-                background: background(layer, "active"),
-            },
-        },
-        separator: {
-            background: borderColor(layer),
-            margin: { top: 2, bottom: 2 },
-        },
+  const legacy_properties = {
+    keystrokeMargin: KEYSTROKE_MARGIN,
+    iconWidth: ICON_SIZE,
+    iconSpacing: ICON_SPACING,
+    item: {
+      ...item,
+      keystroke,
+      active: {
+        background: item.hovered.container.background,
+      },
+      activeHover: {
+        background: item.hovered.container.background,
+      }
     }
+  }
+
+  return {
+    ...legacy_properties,
+    ...container,
+    separator,
+  }
 }
