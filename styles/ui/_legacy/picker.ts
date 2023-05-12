e733409d3f01d71abc79abc2486af2b62aa199c7
Nate Butler <iamnbutler@gmail.com>
2023-05-12 13:34:42 -0400

    WIP picker

--- styles/ui/_legacy/picker.ts
+++ styles/ui/_legacy/picker.ts
@@ -1,85 +1,96 @@
 import { Theme } from "@theme"
 import { containedText } from "@theme/container"
 import { popoverContainerStyle } from "@components/popover"
-import { intensity } from "@theme/intensity/intensity"
+import { addToIntensitySet, intensity } from "@theme/intensity/intensity"
 import { margin, padding } from "@theme/properties"
 import { selectionStyle } from "@theme/properties/selection"
 import { interactiveTextStyle, textStyle } from "@theme/text/text"
+import { useSurfaceIntensity } from "@components/surface"
 
 // TODO: picker -> palette, move to components
 export default function picker(theme: Theme) {
-    const container = popoverContainerStyle({ theme })
+  const container = popoverContainerStyle({ theme })
+  const intensitySet = useSurfaceIntensity(theme, "pane")
+  const activeIntensitySet = addToIntensitySet(intensitySet, 10)
 
-    // TODO: You shouldn't need to specify the color here
-    const itemStyle = interactiveTextStyle(theme, {
-        ...textStyle(theme, { intensity: intensity.secondary }),
-        themeColor: "neutral",
-        padding: padding(4, 12),
-        margin: margin(1, 4, 0, 4),
-        borderRadius: 8,
-    })
+  // TODO: You shouldn't need to specify the color here
+  const itemStyle = interactiveTextStyle(theme, {
+    ...textStyle(theme, { intensity: intensity.secondary }),
+    background: container.background,
+    themeColor: "neutral",
+    padding: padding(12, 4),
+    margin: margin(1, 4, 0, 4),
+    borderRadius: 8,
+  })
 
-    const activeItem = interactiveTextStyle(theme, {
-        ...textStyle(theme),
-        themeColor: "accent",
-        weight: "bold",
-        padding: padding(4, 12),
-        margin: margin(1, 4, 0, 4),
-        borderRadius: 8,
-    })
+  const activeItem = interactiveTextStyle(theme, {
+    ...textStyle(theme, { intensity: intensity.primary }),
+    themeColor: "accent",
+    intensitySet: activeIntensitySet,
+    weight: "bold",
+    padding: padding(12, 4),
+    margin: margin(1, 4, 0, 4),
+    borderRadius: 8,
+  })
 
-    // TODO: Make an input component with placeholder text
-    const input = interactiveTextStyle(theme, {
-        padding: padding(16, 8),
-        margin: margin(0, 0, 4, 0),
-    })
+  // TODO: Make an input component with placeholder text
+  const input = interactiveTextStyle(theme, {
+    padding: padding(16, 8),
+    margin: margin(0, 0, 4, 0),
+  })
 
-    const highlightText = textStyle(theme, {
-        weight: "bold",
-        themeColor: "accent",
-    })
+  const highlightText = textStyle(theme, {
+    weight: "bold",
+    themeColor: "accent",
+  })
 
-    const noMatches = containedText({
-        theme,
-        options: {
-            intensity: intensity.secondary,
-            padding: padding(16, 8),
-        },
-    })
+  const noMatches = containedText({
+    theme,
+    options: {
+      intensity: intensity.secondary,
+      padding: padding(16, 8),
+    },
+  })
 
-    const legacy_properties = {
-        inputEditor: {
-            ...input.default.container,
-            text: input.default.text,
-            placeholderText: textStyle(theme, {
-                intensity: intensity.inactive,
-            }),
-            // TODO: Add player selection color
-            selection: selectionStyle(theme),
-        },
-        emptyInputEditor: {
-            ...input.default.container,
-            text: input.default.text,
-            placeholderText: textStyle(theme, {
-                intensity: intensity.inactive,
-            }),
-            // TODO: Add player selection color
-            selection: selectionStyle(theme),
-        },
-        emptyContainer: {
-            ...container,
-            padding: padding(0),
-        },
-        item: {
-            ...itemStyle.default.container,
-            text: itemStyle.default.text,
-            highlightText,
-            active: activeItem,
-        },
-    }
-    return {
-        ...legacy_properties,
-        container,
-        noMatches,
-    }
+  const paletteContainer = {
+    ...container,
+    cornderRadius: 12,
+    padding: padding(0, 0, 4, 0),
+  }
+
+  const legacy_properties = {
+    inputEditor: {
+      ...input.default.container,
+      text: input.default.text,
+      placeholderText: textStyle(theme, {
+        intensity: intensity.inactive,
+      }),
+      // TODO: Add player selection color
+      selection: selectionStyle(theme),
+    },
+    emptyInputEditor: {
+      ...input.default.container,
+      text: input.default.text,
+      placeholderText: textStyle(theme, {
+        intensity: intensity.inactive,
+      }),
+      // TODO: Add player selection color
+      selection: selectionStyle(theme),
+    },
+    emptyContainer: {
+      ...paletteContainer,
+      padding: padding(0),
+    },
+    item: {
+      ...itemStyle.default.container,
+      text: itemStyle.default.text,
+      highlightText,
+      active: activeItem,
+    },
+  }
+  return {
+    ...paletteContainer,
+    ...legacy_properties,
+    noMatches,
+  }
 }
--- styles/ui/_legacy/workspace.ts
+++ styles/ui/_legacy/workspace.ts
@@ -4,60 +4,58 @@ import json from "./static_json/workspace.json"
 import { useSurfaceIntensity, useSurfaceStyle } from "@components/surface"
 import { borderStyle } from "@theme/properties/border"
 import { padding } from "@theme/properties"
-import { textStyle } from "@theme/text/text"
-import { interactiveTextStyle } from "@theme/text/text"
 import { statusBar } from "./status_bar"
 
 // VERY WIP
 export default function workspace(theme: Theme) {
-    const LOGO_SIZE: Readonly<number> = 256
+  const LOGO_SIZE: Readonly<number> = 256
 
-    const surface = useSurfaceStyle(theme, "background")
-    const pane = useSurfaceStyle(theme, "pane")
+  const surface = useSurfaceStyle(theme, "background")
+  const pane = useSurfaceStyle(theme, "pane")
 
-    // Move to tab_bar.ts
-    const TAB_BAR_HEIGHT: Readonly<number> = 32
+  // Move to tab_bar.ts
+  const TAB_BAR_HEIGHT: Readonly<number> = 32
 
-    const tab_bar = {
-        ...json.tab_bar,
-        height: TAB_BAR_HEIGHT,
-        background: pane.background,
-    }
+  const tab_bar = {
+    ...json.tab_bar,
+    height: TAB_BAR_HEIGHT,
+    background: pane.background,
+  }
 
-    // Move to title_bar.ts
-    const ITEM_SPACING: Readonly<number> = 8
-    const FACEPILE_SPACING: Readonly<number> = 2
+  // Move to title_bar.ts
+  const ITEM_SPACING: Readonly<number> = 8
+  const FACEPILE_SPACING: Readonly<number> = 2
 
-    const title_bar_surface_intensity = useSurfaceIntensity(theme, "background")
+  const title_bar_surface_intensity = useSurfaceIntensity(theme, "background")
 
-    const title_bar = {
-        ...json.titlebar,
-        item_spacing: ITEM_SPACING,
-        face_pile_spacing: FACEPILE_SPACING,
-        // Account for the bottom border
-        height: TAB_BAR_HEIGHT + 1,
-        background: pane.background,
-        border: borderStyle({
-            theme,
-            intensity: title_bar_surface_intensity.border,
-            options: {
-                position: "bottom",
-            },
-        }),
-        padding: padding(0, 8, 0, 80),
-    }
+  const title_bar = {
+    ...json.titlebar,
+    item_spacing: ITEM_SPACING,
+    face_pile_spacing: FACEPILE_SPACING,
+    // Account for the bottom border
+    height: TAB_BAR_HEIGHT + 1,
+    background: pane.background,
+    border: borderStyle({
+      theme,
+      intensity: title_bar_surface_intensity.border,
+      options: {
+        position: "bottom",
+      },
+    }),
+    padding: padding(0, 8, 0, 80),
+  }
 
-    const legacy_properties = {
-        background: surface.background,
-        blank_pane: json.blank_pane,
-        tab_bar,
-        titlebar: title_bar,
-        status_bar: statusBar(theme),
-    }
+  const legacy_properties = {
+    background: surface.background,
+    blank_pane: json.blank_pane,
+    tab_bar,
+    titlebar: title_bar,
+    status_bar: statusBar(theme),
+  }
 
-    return {
-        ...json,
-        ...legacy_properties,
-        pane_divider: surface.border,
-    }
+  return {
+    ...json,
+    ...legacy_properties,
+    pane_divider: surface.border,
+  }
 }
