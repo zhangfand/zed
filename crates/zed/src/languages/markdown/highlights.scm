1aafa157567c84c147566793b13da55c8aa0764d
Nate Butler <iamnbutler@gmail.com>
2023-08-04 11:33:43 -0400

    Add improvements to markdown highlighting
    And additional notes on the state of our markdown highlighting overall.

--- crates/zed/src/languages/markdown/highlights.scm
+++ crates/zed/src/languages/markdown/highlights.scm
@@ -1,10 +1,49 @@
+; Not classified by the grammar:
+; - Strikethrough
+; - Tables
+; - Checklists ([x] and [ ]) (gfm)
+; - @mentions (gfm)
+; - TeX(LaTeX)
+; - Sequence Diagrams
+; - FlowChart
+;
+; Not all of these are vanilla Markdown, but they are commonly used in markdown documents.
+
+; Incorrectly classified by the grammar:
+; - Frontmatter delimiter (---)
+
+; Supported by the grammer but we haven't adapted:
+; - Reference Links
+; - HTML Tags
+
+; Emphasis
+
 (emphasis) @emphasis
 (strong_emphasis) @emphasis.strong
+
+; Headings
 
 [
   (atx_heading)
   (setext_heading)
 ] @title
+
+[
+  (atx_h1_marker)
+  (atx_h2_marker)
+  (atx_h3_marker)
+  (atx_h4_marker)
+  (atx_h5_marker)
+  (atx_h6_marker)
+  (setext_h1_underline)
+  (setext_h2_underline)
+] @punctuation.title
+
+; Prose
+
+[
+    (paragraph)
+] @prose
 
 [
   (list_marker_plus)
@@ -14,11 +53,41 @@
   (list_marker_parenthesis)
 ] @punctuation.list_marker
 
+(block_quote
+  (block_quote_marker) @punctuation.block_quote
+  (paragraph) @prose.block_quote
+)
+
+(thematic_break) @punctuation
+
+; Code
+
 (code_span) @text.literal
 
 (fenced_code_block
   (info_string
-    (language) @text.literal))
+    (language) @text.literal)
+)
 
+(image
+  (image_description) @image_description
+  (link_destination) @image_uri
+)
+
+; Links
+
+(shortcut_link
+  [
+    "["
+    "]"
+  ] @punctuation.link
+)
+
+(uri_autolink) @link_uri
 (link_destination) @link_uri
 (link_text) @link_text
+
+; Special
+
+(backslash_escape) @string.escape
+(entity_reference) @string.special
