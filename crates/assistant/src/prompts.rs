8d7c36a4c3adbde6b43909b4ad3352d948bf4bea
KCaverly <kyle@zed.dev>
2023-09-22 22:26:59 -0400

    progress towards generate prompt for inline assistant

--- crates/assistant/src/prompts.rs
+++ crates/assistant/src/prompts.rs
@@ -101,7 +101,7 @@ fn generate_codegen_planning_prompt(
     writeln!(prompt, "You're an expert {language_name} engineer.\n").unwrap();
 
     // let outline = outline_for_prompt(buffer: &BufferSnapshot, range: Range<language::Anchor>, cx: &AppContext) -> Option<String>;
-    let outline = outline_for_prompt(buffer, range, cx);
+    let outline = outline_for_prompt(buffer, range.clone(), cx);
     if let Some(outline) = outline {
         writeln!(
             prompt,
@@ -111,7 +111,11 @@ fn generate_codegen_planning_prompt(
     }
 
     // Assume for now that we are just generating
-    writeln!(prompt, "In particular, the user's cursor is currently on the '<||>' span in the above outline, with no text selected.").unwrap();
+    if range.clone().start == range.end {
+        writeln!(prompt, "In particular, the user's cursor is current on the '<||>' span in the above outline, with no text selected.").unwrap();
+    } else {
+        writeln!(prompt, "In particular, the user has selected a section of the text between the '<|' and '|>' spans.").unwrap();
+    }
 
     writeln!(
         prompt,
@@ -127,6 +131,83 @@ fn generate_codegen_planning_prompt(
     prompt
 }
 
+fn generate_content_prompt(
+    user_prompt: String,
+    snippets: Vec<String>,
+    language_name: Arc<str>,
+    buffer: &BufferSnapshot,
+    range: Range<language::Anchor>,
+    cx: &AppContext,
+) -> String {
+    // Language name should always be refered to in all lowercase
+    let language_name = language_name.to_lowercase();
+
+    let mut prompt = String::new();
+
+    // General Preamble
+    // It would be interesting to see, if there is any improvement
+    // putting this in a "System" prompt
+    writeln!(prompt, "You're an expert {language_name} engineer.\n").unwrap();
+
+    if snippets.len() > 0 {
+        writeln!(prompt, "You are currently working inside a large repository, here are a few additional code snippets which may provide useful context.").unwrap();
+    }
+    for snippet in &snippets {
+        writeln!(prompt, "{snippet}").unwrap();
+    }
+
+    // let outline = outline_for_prompt(buffer: &BufferSnapshot, range: Range<language::Anchor>, cx: &AppContext) -> Option<String>;
+    let outline = outline_for_prompt(buffer, range.clone(), cx);
+    if let Some(outline) = outline {
+        writeln!(
+        prompt,
+        "The file you are currently working on has the following outline:\n```{language_name}\n{outline}\n```"
+    )
+    .unwrap();
+    }
+
+    // Assume for now that we are just generating
+    if range.clone().start == range.end {
+        writeln!(prompt, "In particular, the user's cursor is current on the '<||>' span in the above outline, with no text selected.").unwrap();
+    } else {
+        writeln!(prompt, "In particular, the user has selected a section of the text between the '<|' and '|>' spans.").unwrap();
+    }
+
+    if range.clone().start == range.end {
+        writeln!(
+            prompt,
+            "Modify the users code selected text based upon the users prompt: {user_prompt}"
+        )
+        .unwrap();
+        writeln!(
+            prompt,
+            "You MUST reply with only the adjusted code, not the entire file."
+        );
+    } else {
+        writeln!(
+            prompt,
+            "Assume the cursor is located where the `<|` marker is."
+        )
+        .unwrap();
+        writeln!(
+            prompt,
+            "Text can't be replaced, so assume your answer will be inserted at the cursor."
+        )
+        .unwrap();
+        writeln!(
+            prompt,
+            "Generate text based on the users prompt: {user_prompt}"
+        )
+        .unwrap();
+    }
+
+    writeln!(prompt, "Your answer MUST always be valid {language_name}").unwrap();
+    writeln!(prompt, "Always wrap your response in a Markdown codeblock").unwrap();
+    writeln!(prompt, "Never make remarks about the output.").unwrap();
+
+    prompt
+}
+
 #[cfg(test)]
 pub(crate) mod tests {
 
@@ -135,6 +216,7 @@ pub(crate) mod tests {
     use super::*;
     use crate::{assistant_panel::tests::rust_lang, MessageId};
     use ai::{function_calling::OpenAIFunction, RequestMessage};
+    use collections::HashMap;
     use gpui::AppContext;
     use indoc::indoc;
     use language::{language_settings, tree_sitter_rust, Buffer, Language, LanguageConfig, Point};
@@ -300,7 +382,7 @@ pub(crate) mod tests {
         );
     }
 
-    // // WIP: This is here just to test the entrypoint for function calling.
+    // WIP: This is here just to test the entrypoint for function calling.
     // #[gpui::test]
     // fn test_planning_prompt(cx: &mut AppContext) {
     //     let api_key: String = env::var("OPENAI_API_KEY").ok().unwrap();
@@ -334,7 +416,7 @@ pub(crate) mod tests {
     //     let buffer = cx.add_model(|cx| Buffer::new(0, 0, text).with_language(language.clone(), cx));
     //     let snapshot = buffer.read(cx).snapshot();
     //     let range =
-    //         snapshot.anchor_before(Point::new(6, 0))..snapshot.anchor_before(Point::new(6, 0));
+    //         snapshot.anchor_before(Point::new(7, 12))..snapshot.anchor_before(Point::new(20, 0));
 
     //     // let user_prompt =
     //     //     "Please create a new function which adds together the result of the a and b functions"
@@ -359,11 +441,25 @@ pub(crate) mod tests {
     //         Box::new(RepositoryContextRetriever),
     //     ];
 
+    //     let function_map = functions
+    //         .into_iter()
+    //         .map(|function| (function.name(), function))
+    //         .collect::<HashMap<String, Box<dyn OpenAIFunction>>>();
+
     //     let provider = OpenAIFunctionCallingProvider::new(api_key);
     //     let data = provider.complete("gpt-4-0613".to_string(), messages, functions);
 
     //     let data = smol::future::block_on(data).unwrap();
-    //     dbg!(data);
+
+    //     // let function_name = &data.name;
+    //     // if let Some(function) = function_map.get(function_name) {
+    //     //     match function_name.as_str() {
+    //     //         "rewrite_prompt" => {
+    //     //             // let prompt = function.complete(data.arguments);
+    //     //         }
+    //     //         "retrieve_context_from_repository" => {}
+    //     //     }
+    //     // }
 
     //     panic!();
     // }
