c76bbcdd29243ccbb1238e8fbe0ab65fba68e1ea
Nate Butler <iamnbutler@gmail.com>
2023-10-24 15:51:57 -0400

    Refactor theme_system.rs to use Hsla directly instead of PaletteHsla

--- crates/ui2/src/theme_system.rs
+++ crates/ui2/src/theme_system.rs
@@ -1,26 +1,25 @@
 // https://www.figma.com/community/plugin/1105513882835626049
 
 use gpui2::{SharedString, Hsla, hsla};
-use palette::{Hsla as PaletteHsla};
 
 
-pub fn palette_hsla_to_hsla(palette_hsla: PaletteHsla) -> Hsla {
-    let hue = palette_hsla.color.hue.to_positive_degrees() / 360.0;
-    let saturation = palette_hsla.color.saturation;
-    let lightness = palette_hsla.color.lightness;
-    let alpha = palette_hsla.alpha;
+// pub fn palette_hsla_to_hsla(palette_hsla: PaletteHsla) -> Hsla {
+//     let hue = palette_hsla.color.hue.to_positive_degrees() / 360.0;
+//     let saturation = palette_hsla.color.saturation;
+//     let lightness = palette_hsla.color.lightness;
+//     let alpha = palette_hsla.alpha;
 
-    hsla(hue as f32, saturation as f32, lightness as f32, alpha as f32)
-}
+//     hsla(hue as f32, saturation as f32, lightness as f32, alpha as f32)
+// }
 
-pub fn hsla_to_palette_hsla(hsla: Hsla) -> PaletteHsla {
-    let hue = hsla.h * 360.0;
-    let saturation = hsla.s as f32;
-    let lightness = hsla.l as f32;
-    let alpha = hsla.a as f32;
+// pub fn hsla_to_palette_hsla(hsla: Hsla) -> PaletteHsla {
+//     let hue = hsla.h * 360.0;
+//     let saturation = hsla.s as f32;
+//     let lightness = hsla.l as f32;
+//     let alpha = hsla.a as f32;
 
-    PaletteHsla::new(hue, saturation, lightness, alpha)
-}
+//     PaletteHsla::new(hue, saturation, lightness, alpha)
+// }
 
 #[derive(Debug, Clone)]
 pub enum ColorScale {
@@ -106,12 +105,12 @@ impl ColorScale {
 #[derive(Debug, Clone)]
 pub struct Color {
     pub name: String,
-    pub hsla: PaletteHsla,
+    pub hsla: Hsla,
     pub scale: ColorScale,
 }
 
 impl Color {
-    pub fn new<S: Into<String>>(name: S, hsla: PaletteHsla, scale: ColorScale) -> Self {
+    pub fn new<S: Into<String>>(name: S, hsla: Hsla, scale: ColorScale) -> Self {
         Self {
             name: name.into(),
             hsla,
@@ -119,12 +118,12 @@ impl Color {
         }
     }
 
-    pub fn palette_hsla(&self) -> PaletteHsla {
-        self.hsla
+    pub fn new_name_from_index(index: usize, hsla: Hsla, scale: ColorScale) -> String {
+        format!("{} {}", scale.name(), index)
     }
 
     pub fn hsla(&self) -> Hsla {
-        palette_hsla_to_hsla(self.hsla)
+        self.hsla
     }
 
     pub fn scale(&self) -> ColorScale {
@@ -139,7 +138,7 @@ pub struct Scale {
 }
 
 impl Scale {
-    pub fn new(name: ColorScale, hues: [PaletteHsla; 12]) -> Self {
+    pub fn new(name: ColorScale, hues: [Hsla; 12]) -> Self {
         let steps = hues.iter().enumerate().map(|(i, &hue)| {
             let step = i + 1;
             let color_name = format!("{:?} {}", name, step);
@@ -172,14 +171,14 @@ impl CustomScale {
         CustomScaleBuilder::new(name)
     }
 
-    pub fn closest_scale_index(hsla_color: PaletteHsla) -> usize {
+    pub fn closest_scale_index(hsla_color: Hsla) -> usize {
        let default_scale = CustomScale::default();
        let mut best_match = 0;
        let mut best_score = f32::MIN;
 
        for (index, scale_color) in default_scale.steps.iter().enumerate() {
-          let lum_diff = (hsla_color.color.lightness - scale_color.hsla.color.lightness).abs();
-          let sat_diff = (hsla_color.color.saturation - scale_color.hsla.color.saturation).abs();
+          let lum_diff = (hsla_color.l - scale_color.hsla.l).abs();
+          let sat_diff = (hsla_color.s - scale_color.hsla.s).abs();
           // Essentially magic numbers, Luminoisty is more visually important to the scale
           // than saturation so we weight it higher
           let score = (5.0 * lum_diff) + (3.0 * sat_diff);
@@ -191,23 +190,47 @@ impl CustomScale {
        }
        best_match
     }
+
+    pub fn custom_scale_from_hsla(name: String, hsla: Hsla) -> CustomScale {
+        let index = CustomScale::closest_scale_index(hsla);
+        let input_step_name = Color::new_name_from_index(index, hsla, ColorScale::Custom(name.clone()));
+        let input_step = Color::new(input_step_name, hsla, ColorScale::Custom(name.clone()));
+
+        let mut steps = CustomScale::default().steps.clone();
+
+            steps[index] = input_step;
+
+        if index > 0 {
+            steps[index - 1].hsla.h = steps[index].hsla.h;
+        }
+        if index < 11 {
+            steps[index + 1].hsla.h = steps[index].hsla.h;
+        }
+
+        let hsla_steps: [Hsla; 12] = steps.map(|color| color.hsla).collect();
+        let custom_scale = CustomScale::builder(&name)
+                    .hues(hsla_steps)
+                    .build();
+
+        custom_scale
+    }
 }
 
 impl Default for CustomScale {
     fn default() -> Self {
-        let hues: [PaletteHsla; 12] = [
-            hsla_to_palette_hsla(hsla(0.0, 1.00, 0.99, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 1.00, 0.98, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.90, 0.96, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 1.00, 0.93, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 1.00, 0.90, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.94, 0.87, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.77, 0.81, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.70, 0.74, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.75, 0.59, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.69, 0.55, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.65, 0.49, 1.0)),
-            hsla_to_palette_hsla(hsla(0.0, 0.63, 0.24, 1.0)),
+        let hues: [Hsla; 12] = [
+            hsla(0.0, 1.00, 0.99, 1.0),
+            hsla(0.0, 1.00, 0.98, 1.0),
+            hsla(0.0, 0.90, 0.96, 1.0),
+            hsla(0.0, 1.00, 0.93, 1.0),
+            hsla(0.0, 1.00, 0.90, 1.0),
+            hsla(0.0, 0.94, 0.87, 1.0),
+            hsla(0.0, 0.77, 0.81, 1.0),
+            hsla(0.0, 0.70, 0.74, 1.0),
+            hsla(0.0, 0.75, 0.59, 1.0),
+            hsla(0.0, 0.69, 0.55, 1.0),
+            hsla(0.0, 0.65, 0.49, 1.0),
+            hsla(0.0, 0.63, 0.24, 1.0),
         ];
         Self::builder("Untitled Custom Scale").hues(hues).build()
     }
@@ -216,7 +239,7 @@ impl Default for CustomScale {
 #[derive(Debug, Clone)]
 pub struct CustomScaleBuilder {
     pub name: String,
-    pub hues: Option<[PaletteHsla; 12]>,
+    pub hues: Option<[Hsla; 12]>,
 }
 
 impl CustomScaleBuilder {
@@ -227,7 +250,7 @@ impl CustomScaleBuilder {
         }
     }
 
-    pub fn hues(mut self, hues: [PaletteHsla; 12]) -> Self {
+    pub fn hues(mut self, hues: [Hsla; 12]) -> Self {
         self.hues = Some(hues);
         self
     }
