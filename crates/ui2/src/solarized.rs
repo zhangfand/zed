1a46df7243c14ceffa6946e4ff69416660af47fa
Nate Butler <iamnbutler@gmail.com>
2023-10-26 01:01:41 -0400

    Refactor solarized theme to use ColorScale instead of CustomScale

--- crates/ui2/src/solarized.rs
+++ crates/ui2/src/solarized.rs
@@ -1,27 +1,27 @@
 // A example theme for building out theme2
 
-use crate::{theme2::{Theme, ThemeVariant, Appearance, RequiredScales}, ScaleType, ThemeColor, NewCustomScale, CustomScale};
+use crate::theme2::{Theme, ThemeVariant, Appearance, RequiredScales, ColorScale, ColorScaleName};
 
 struct SolarizedScale {
-    pub neutral: CustomScale,
-    pub neutral_dark: CustomScale,
-    pub yellow: CustomScale,
-    pub orange: CustomScale,
-    pub red: CustomScale,
-    pub magenta: CustomScale,
-    pub violet: CustomScale,
-    pub blue: CustomScale,
-    pub cyan: CustomScale,
-    pub green: CustomScale,
+    pub neutral: ColorScale,
+    pub neutral_dark: ColorScale,
+    pub yellow: ColorScale,
+    pub orange: ColorScale,
+    pub red: ColorScale,
+    pub magenta: ColorScale,
+    pub violet: ColorScale,
+    pub blue: ColorScale,
+    pub cyan: ColorScale,
+    pub green: ColorScale,
 }
 
 pub fn solarized() -> Theme {
     let scale = SolarizedScale {
-        neutral: NewCustomScale::from_8_hex(
+        neutral: ColorScale::from_8_hex(
             "Neutral",
             ["#fdf6e3","#eee8d5","#93a1a1","#839496","#657b83","#586e75","#073642","#002b36",
         ]),
-        neutral_dark: NewCustomScale::from_8_hex(
+        neutral_dark: ColorScale::from_8_hex(
             "Neutral",
             [
                 "#002b36",
@@ -34,60 +34,58 @@ pub fn solarized() -> Theme {
                 "#fdf6e3",
             ]
         ),
-        yellow: NewCustomScale::from_hex("Yellow", "#b58900"),
-        orange: NewCustomScale::from_hex("Orange", "#cb4b16"),
-        red: NewCustomScale::from_hex("Red", "#dc322f"),
-        magenta: NewCustomScale::from_hex("Magenta", "#d33682"),
-        violet: NewCustomScale::from_hex("Violet", "#6c71c4"),
-        blue: NewCustomScale::from_hex("Blue", "#268bd2"),
-        cyan: NewCustomScale::from_hex("Cyan", "#2aa198"),
-        green: NewCustomScale::from_hex("Green", "#859900")
+        yellow: ColorScale::from_hex("Yellow", "#b58900"),
+        orange: ColorScale::from_hex("Orange", "#cb4b16"),
+        red: ColorScale::from_hex("Red", "#dc322f"),
+        magenta: ColorScale::from_hex("Magenta", "#d33682"),
+        violet: ColorScale::from_hex("Violet", "#6c71c4"),
+        blue: ColorScale::from_hex("Blue", "#268bd2"),
+        cyan: ColorScale::from_hex("Cyan", "#2aa198"),
+        green: ColorScale::from_hex("Green", "#859900")
     };
 
      let solarized_light = ThemeVariant {
         id: 0,
         name: "Solarized Light".into(),
         appearance: Appearance::Light,
-        scales: (
-            RequiredScales {
-                neutral: ScaleType::Custom(scale.neutral.clone()),
-                accent: ScaleType::Custom(scale.blue.clone()),
-                positive: ScaleType::Custom(scale.green.clone()),
-                negative: ScaleType::Custom(scale.red.clone()),
-                caution: ScaleType::Custom(scale.orange.clone()),
-            },
+        scales: RequiredScales {
+            neutral: scale.neutral.clone(),
+            accent: scale.blue.clone(),
+            positive: scale.green.clone(),
+            negative: scale.red.clone(),
+            caution: scale.orange.clone(),
+        },
+        extended_scales:
             vec![
-                ScaleType::Custom(scale.yellow.clone()),
-                ScaleType::Custom(scale.magenta.clone()),
-                ScaleType::Custom(scale.violet.clone()),
-                ScaleType::Custom(scale.cyan.clone()),
+                scale.yellow.clone(),
+                scale.magenta.clone(),
+                scale.violet.clone(),
+                scale.cyan.clone()
             ],
-        ),
         // TODO: Populate this using RequiredScales
-        color: ThemeColor::new()
+        color: None
     };
 
      let solarized_dark = ThemeVariant {
         id: 1,
         name: "Solarized Dark".into(),
         appearance: Appearance::Dark,
-        scales: (
-            RequiredScales {
-                neutral: ScaleType::Custom(scale.neutral_dark.clone()),
-                accent: ScaleType::Custom(scale.blue.clone()),
-                positive: ScaleType::Custom(scale.green.clone()),
-                negative: ScaleType::Custom(scale.red.clone()),
-                caution: ScaleType::Custom(scale.orange.clone()),
-            },
+        scales: RequiredScales {
+            neutral: scale.neutral_dark.clone(),
+            accent: scale.blue.clone(),
+            positive: scale.green.clone(),
+            negative: scale.red.clone(),
+            caution: scale.orange.clone(),
+        },
+        extended_scales:
             vec![
-                ScaleType::Custom(scale.yellow.clone()),
-                ScaleType::Custom(scale.magenta.clone()),
-                ScaleType::Custom(scale.violet.clone()),
-                ScaleType::Custom(scale.cyan.clone()),
-            ]
-        ),
+                scale.yellow.clone(),
+                scale.magenta.clone(),
+                scale.violet.clone(),
+                scale.cyan.clone()
+            ],
         // TODO: Populate this using RequiredScales
-        color: ThemeColor::new()
+        color: None
     };
 
     let theme = Theme::new(
