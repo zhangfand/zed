b897a767ceaa9039397e0250c0d4cc92a3c419fa
Marshall Bowers <elliott.codes@gmail.com>
2024-02-16 16:27:16 -0500

    Tidy

--- crates/scripting/src/scripting.rs
+++ crates/scripting/src/scripting.rs
@@ -1,84 +1,12 @@
 use anyhow::{anyhow, Context, Result};
-use gpui::{AppContext, Global};
-use serde::{Deserialize, Serialize};
+use gpui::AppContext;
+use serde::Deserialize;
 use serde_v8::Serializable;
 
-// https://github.com/matejkoncal/v8-experiments/blob/master/src/main.rs
-// pub fn
-//
-pub struct ScriptModule {
-    exports: v8::Global<v8::Object>,
-}
-
-// struct V8Platform(v8::SharedRef<v8::Platform>);
-
-// struct GlobalV8Platform(V8Platform);
-
-// impl Global for GlobalV8Platform {}
-
-// struct GlobalV8Isolate(v8::Isolate);
-
-pub fn init(cx: &mut AppContext) {
+pub fn init(_cx: &mut AppContext) {
     let platform = v8::new_default_platform(0, false).make_shared();
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
-
-    // cx.set_global(GlobalV8Platform(V8Platform(platform)));
-
-    //
-    // cx.set_global(GlobalV8Isolate(v8::Isolate::new(Default::default())));
-
-    let isolate = &mut v8::Isolate::new(Default::default());
-    let scope = &mut v8::HandleScope::new(isolate);
-    let context = v8::Context::new(scope);
-    let scope = &mut v8::ContextScope::new(scope, context);
-
-    let first_function = v8::Function::new(
-        scope,
-        |scope: &mut v8::HandleScope,
-         args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            let arg = args.get(0);
-            let arg_string = arg.to_string(scope).unwrap().to_rust_string_lossy(scope);
-            println!("passed from JS to rust: {}", arg_string);
-            let returned_value_string =
-                v8::String::new(scope, "This is returned from rust to javascript")
-                    .unwrap()
-                    .into();
-            rv.set(returned_value_string);
-        },
-    )
-    .unwrap()
-    .into();
-
-    let name = v8::String::new(scope, "testFunction").unwrap().into();
-    let global = context.global(scope);
-    global.set(scope, name, first_function);
-    // global.in
-
-    // v8::script_compiler::compile_module
-
-    let code = v8::String::new(scope, "const x = 'foo '; x + testFunction('bar') ").unwrap();
-    let script = v8::Script::compile(scope, code, None).unwrap();
-    let result = script.run(scope).unwrap().to_rust_string_lossy(scope);
-    dbg!(&result);
-
-    #[derive(Serialize)]
-    struct TestObj {
-        pub age: u32,
-    }
-
-    let args: Vec<Box<dyn Serializable>> =
-        vec![Box::new(TestObj { age: 73 }), Box::new(TestObj { age: 42 })];
-
-    let result: u32 = run_script("function foo(a, b) { return a.age; }", "foo", args).unwrap();
-
-    dbg!(&result);
-
-    // v8::Global::new(isolate, script);
-
-    // scope.
-    // cx.set_global(Arc::new())
 }
 
 pub fn run_script<'de, T: Deserialize<'de>>(
@@ -95,7 +23,7 @@ pub fn run_script<'de, T: Deserialize<'de>>(
         .ok_or_else(|| anyhow!("failed to initialize V8 string for script"))?;
     let script = v8::Script::compile(scope, code, None)
         .ok_or_else(|| anyhow!("failed to compile script"))?;
-    let result = script
+    script
         .run(scope)
         .ok_or_else(|| anyhow!("failed to run script"))?;
 
