19b2e1b22f90c8294973f43757eb9b62ee51491c
Nathan Sobo <nathan@zed.dev>
2024-02-21 19:37:10 -0700

    Remove use of z-index from FacePile

--- crates/collab_ui/examples/face_pile.rs
+++ crates/collab_ui/examples/face_pile.rs
@@ -9,7 +9,7 @@ impl Render for FacepileExample {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .size_full()
-            .bg(rgb(0x2e7d32))
+            .bg(rgb(0x555555))
             .child(FacePile::new(smallvec![
                 Avatar::new("https://avatars.githubusercontent.com/nathansobo").into_any_element(),
                 Avatar::new("https://avatars.githubusercontent.com/as-cii").into_any_element(),
